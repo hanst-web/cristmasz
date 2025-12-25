8bdfcf628a1c410a863ce90d32dd80804b0c1ce6
hanst-web <farelindra530@gmail.com>
2025-12-26 08:05:49 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // Tambahkan baris base di bawah ini sesuai nama repositori Anda
+  base: '/cristmasz/', 
 })
